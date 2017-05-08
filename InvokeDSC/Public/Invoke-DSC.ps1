c78dd36acb8c7f5067722bfec1601deca0558d26
Joshua Duffney <JDuffney@paylocity.com>
2017-05-08 08:22:11 -0500

    added write-output for test and set

--- InvokeDSC/Public/Invoke-DSC.ps1
+++ InvokeDSC/Public/Invoke-DSC.ps1
@@ -15,23 +15,24 @@ function Invoke-DSC
         foreach ($resource in $resources) {
 
             try {
-                Write-Output "Running Test Method"
-
+                
                 $splat = @{
                     Name = $resource.dscResourceName
                     Property = $resource.Property
                     ModuleName = $resource.ModuleName
                     ErrorAction = 'SilentlyContinue'
                 }
-
-                $testResults = Invoke-DscResource @splat -Method Test -ErrorVariable TestError
+                
+                Write-Output "[Start Test] [[$($resource.dscResourceName)]$($resource.ResourceName)]"
+                $testResults = Invoke-DscResource @splat -Method Test -ErrorVariable TestError -Verbose:$false | Out-Null
 
                 if ($TestError) {
                     Write-Error ("Failed to Invoke $($resource.resourceName)" + ($TestError[0].Exception.Message))
                 }
 
                 elseif (($testResults.InDesiredState) -ne $true) {
-                    Invoke-DscResource @splat -Method Set -ErrorVariable SetError
+                    Write-Output "[Start Set] [[$($resource.dscResourceName)]$($resource.ResourceName)]"
+                    Invoke-DscResource @splat -Method Set -ErrorVariable SetError -Verbose:$false | Out-Null
                 }
 
                 if ($SetError) {
